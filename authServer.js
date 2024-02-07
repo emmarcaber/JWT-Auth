dc49be37445dd54aa1573e70f841bd7e3df35106
Emmar Caber <caberemmar@gmail.com>
2024-02-08 07:09:21 +0800

    create logout route to remove refreshToken

--- authServer.js
+++ authServer.js
@@ -13,7 +13,7 @@ app.post("/token", (req, res) => {
   const refreshToken = req.body.token;
 
   if (refreshToken == null) return res.sendStatus(401);
-  if (!refreshToken.includes(refreshToken)) return res.sendStatus(403);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
 
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
@@ -21,6 +21,11 @@ app.post("/token", (req, res) => {
 
     res.json({ accessToken: accessToken });
   });
+});
+
+app.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
 });
 
 app.post("/login", (req, res) => {
@@ -32,7 +37,10 @@ app.post("/login", (req, res) => {
   const accessToken = generateAccessToken(user);
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
   refreshTokens.push(refreshToken);
-  res.json({ accessToken: accessToken, refreshToken: refreshToken });
+  res.json({
+    accessToken: accessToken,
+    refreshToken: refreshToken,
+  });
 });
 
 function generateAccessToken(user) {
