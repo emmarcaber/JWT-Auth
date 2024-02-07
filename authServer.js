7b3341f55f6770c17da533155bacab31239c825d
Emmar Caber <caberemmar@gmail.com>
2024-02-08 06:48:09 +0800

    generate refresh tokens

--- authServer.js
+++ authServer.js
@@ -7,6 +7,22 @@ const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
+let refreshTokens = [];
+
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshToken.includes(refreshToken)) return res.sendStatus(403);
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+
+    res.json({ accessToken: accessToken });
+  });
+});
+
 app.post("/login", (req, res) => {
   // Authenticate User
   const username = req.body.username;
@@ -15,6 +31,7 @@ app.post("/login", (req, res) => {
 
   const accessToken = generateAccessToken(user);
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
   res.json({ accessToken: accessToken, refreshToken: refreshToken });
 });
 
